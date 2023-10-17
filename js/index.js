17ff725477e499559e4cf12f04a97225238bd79d
anndinh0895 <133069610+anndinh0895@users.noreply.github.com>
2023-10-17 18:20:03 -0500

    UX_UI_HW_19
    Includes documents from previous homeworks and addition of Jquery file that scripts hover over location symbols

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(".greenville").hover(function () {
+    $(".greenville").css({"color":"#5899C5", "transform":"scale(1.2)", "text-shadow":"2px 2px 3px black"})
+}, 
+function () {
+    $(".greenville").css({"color":"black", "transform":"scale(1)", "text-shadow":"0px 0px 0px white"});
+});
+
+$(".knox").hover(function () {
+    $(".knox").css({"color":"#5899C5", "transform":"scale(1.2)", "text-shadow":"2px 2px 3px black"})
+}, 
+function () {
+    $(".knox").css({"color":"black", "transform":"scale(1)", "text-shadow":"0px 0px 0px white"});
+});
+
+$(".oaklawn").hover(function () {
+    $(".oaklawn").css({"color":"#5899C5", "transform":"scale(1.2)", "text-shadow":"2px 2px 3px black"})
+}, 
+function () {
+    $(".oaklawn").css({"color":"black", "transform":"scale(1)", "text-shadow":"0px 0px 0px white"});
+});
+
+$(".bishop").hover(function () {
+    $(".bishop").css({"color":"#5899C5", "transform":"scale(1.2)", "text-shadow":"2px 2px 3px black"})
+}, 
+function () {
+    $(".bishop").css({"color":"black", "transform":"scale(1)", "text-shadow":"0px 0px 0px white"});
+});
